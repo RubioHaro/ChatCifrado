6439133e3ac244ac3503c7c94a462805dc6390a2
Rubio Haro <chavo0022009@gmail.com>
2016-10-16 18:33:31 -0500

    Add files via upload

---
+++ ChatConDes/src/chatcondes/ChatConDes.java
@@ -0,0 +1,24 @@
+/*
+ * New Ligths
+ * Services info Web
+ * Rubio Haro 
+ */
+package chatcondes;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Rod
+ */
+public class ChatConDes {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {                 
+    
+    }
+    
+}
